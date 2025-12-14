2c6b0e4ff150176702c4302803e49cb9ee5ef13d
anyej <anyej@BOOK-98H9DE9UB3>
2025-12-14 23:38:07 +0900

    Modify A.java and B.java in Bob branch.

--- GitAYJ/src/mygit/B.java
+++ GitAYJ/src/mygit/B.java
@@ -3,5 +3,7 @@ package mygit;
 public class B {
 	public static void main(String[] args) { 
 		System.out.println("This is B.java.");
+		
+		System.out.println("Hi! This is B.java in Bob branch.");
 	}
 }
